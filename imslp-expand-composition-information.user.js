0d9e0c24a78feee24e7a07098f2d5d34ff789049
Enchoseon <38841444+Enchoseon@users.noreply.github.com>
2021-11-05 21:04:54 -0700

    Update imslp-expand-composition-information.user.js

--- imslp-expand-composition-information.user.js
+++ imslp-expand-composition-information.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name        IMSLP Expand Composition Information
 // @namespace   https://github.com/Enchoseon/enchos-assorted-userscripts/blob/main/imslp-expand-composition-information.user.js
-// @version     1.0.0
+// @version     1.0.1
 // @description Expands composition information in the IMSLP library.
 // @author      Enchoseon
 // @include     *imslp.org/wiki/Category:*
@@ -11,10 +11,31 @@
 
 (function() {
     "use strict";
-    Object.values(document.getElementById("mw-pages").getElementsByTagName("li")).forEach((elem) => {
-        elem.dispatchEvent(new Event("mouseover"));
-        window.addEventListener("mouseout", function (event) {
-            event.stopPropagation();
-        }, true);
-    });
+    // ==============================
+    // Expand Composition Information
+    // ==============================
+    expandCompositionInformation();
+    document.getElementById("cathassecselp1").addEventListener("click", function (event) {
+        expandCompositionInformation();
+    }, true);
+    function expandCompositionInformation() {
+        Object.values(document.getElementById("mw-pages").getElementsByTagName("li")).forEach((elem) => {
+            elem.dispatchEvent(new Event("mouseover"));
+            window.addEventListener("mouseout", function (event) {
+                event.stopPropagation();
+            }, true);
+        });
+    }
+    // ============
+    // Style Tweaks
+    // ============
+    const css = `
+        .showHSList a {
+            font-weight: bold;
+        }
+    `;
+    var s = document.createElement("style");
+    s.setAttribute("type", "text/css");
+    s.appendChild(document.createTextNode(css));
+    document.getElementsByTagName("head")[0].appendChild(s);
 })();
