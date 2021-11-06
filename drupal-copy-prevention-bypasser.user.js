a019756279b3866f5484fe5e05122921b02cf2ab
Enchoseon <38841444+Enchoseon@users.noreply.github.com>
2021-11-05 17:57:04 -0700

    Update and rename drupal-copy-prevention-bypasser.js to drupal-copy-prevention-bypasser.user.js

---
+++ drupal-copy-prevention-bypasser.user.js
@@ -0,0 +1,20 @@
+// ==UserScript==
+// @name     	Drupal Copy Prevention Bypasser
+// @namespace   https://github.com/Enchoseon/enchos-assorted-userscripts/blob/main/drupal-copy-prevention-bypasser.user.js
+// @version  	1.0.0
+// @description Bypass Drupal Copy Prevention on sites that use it.
+// @author   	Enchoseon
+// @include  	*
+// @grant    	none
+// ==/UserScript==
+
+(function() {
+    "use strict";
+    if (typeof Drupal !== "undefined" && typeof Drupal.behaviors.copyprevention !== "undefined") {
+        console.log("Drupal site using copyprevention detected");
+        Object.values(document.getElementsByClassName("copyprevention-processed")).forEach((elem) => {
+            elem.style.zIndex = "69420";
+            elem.replaceWith(elem.cloneNode(true)); // Extremely unclean removal of event listeners
+        });
+    }
+})();
