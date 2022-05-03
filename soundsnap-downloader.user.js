413e4e2b3a260779f7a4f29ad084c7e038229e9c
Encho <38841444+Enchoseon@users.noreply.github.com>
2022-05-03 00:21:26 -0700

    Create soundsnap-downloader.user.js

---
+++ soundsnap-downloader.user.js
@@ -0,0 +1,60 @@
+// ==UserScript==
+// @name        Soundsnap Downloader
+// @namespace   https://github.com/Enchoseon/enchos-assorted-userscripts/raw/main/soundsnap-downloader.user.js
+// @version     0.6.9
+// @description Add download link to Soundsnap previews.
+// @author      Enchoseon
+// @include     *soundsnap.com/search/audio?query=*
+// @grant       none
+// @run-at      document-start
+// ==/UserScript==
+
+(function() {
+    "use strict";
+    // =================
+    // Mutation Observer
+    // =================
+    var flag = false;
+    const observer = new MutationObserver(function(mutations_list) {
+        mutations_list.forEach(function(mutation) {
+            mutation.addedNodes.forEach(function(added_node) {
+                if (added_node.className === "ojoo-teaser") {
+                    observer.disconnect();
+                    if (flag) {
+                        return;
+                    } else {
+                        flag = true;
+                        start();
+                    }
+                }
+            });
+        });
+    });
+    observer.observe(document, { subtree: true, childList: true });
+    function start() {
+        // =========================
+        // Intercept Download Button
+        // =========================
+        const tracks = document.querySelectorAll("div.ojoo-teaser");
+        tracks.forEach((track) => {
+            const downloadButton = track.querySelector("a.ss_icons.ss_download");
+            console.log(downloadButton);
+            downloadButton.removeAttribute("href");
+            downloadButton.addEventListener("click", () => {
+                var event = new MouseEvent("mouseover", {
+                    "bubbles": true,
+                    "cancelable": true
+                });
+                track.querySelector("span.ojoo-play").dispatchEvent(event);
+                downloadTrack(track.querySelector("audio").src, track.querySelector("div.audio-description").innerHTML);
+            });
+        });
+        // =================
+        // Download Function
+        // =================
+        function downloadTrack(url, name) {
+            console.log("Downloading: " + name + " (" + url + ")");
+            window.open(url);
+        }
+    }
+})();
